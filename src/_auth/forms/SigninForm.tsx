9428a6a8fd1ac7ef8cbaeedee90c6a79c3ca4dd3
Harshil Lakhani <harshillakhani2264@gmail.com>
2024-12-03 03:53:44 +0530

    Sign Up form UI & Authentication connection completed

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SigninForm = () => {
+  return (
+    <div>
+      SignIn form
+    </div>
+  )
+}
+
+export default SigninForm
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod"
+
+
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {
+        message: " Too Short"
+    }),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    email: z.string(),
+    password: z.string().min(8, {
+        message: "Atleast 8 characters"
+    })
+  })
